51269fdd71662a995a64ab449fb3df8192c977b7
Josef Liska <josef.liska@virtualmaster.com>
2018-06-27 10:13:17 +0200

    Hack to force accept header to be application/json in order to be used as flexirest adapter. Do not know how to configure flexirest to use this accept header

--- lib/typhoeus/adapters/faraday.rb
+++ lib/typhoeus/adapters/faraday.rb
@@ -121,7 +121,7 @@ module Faraday # :nodoc:
         opts = {
           :method => env[:method],
           :body => env[:body],
-          :headers => env[:request_headers]
+          :headers => env[:request_headers].merge('accpet' => 'application/json')
         }.merge(@adapter_options)
 
         ::Typhoeus::Request.new(env[:url].to_s, opts)
